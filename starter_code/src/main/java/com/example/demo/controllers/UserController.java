bf73d80cc90eecc0b67e7f54fab7654e3abcea59
Ansgar Nell <fixed-term.Ansgar.Nell@de.bosch.com>
2019-10-20 21:01:37 +0200

    Optimize imports.

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
